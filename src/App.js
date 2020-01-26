3c6ada604f56afaa850140bc24f9a386e6b5367a
Marco Wong <marco@Marcos-Air.frontierlocal.net>
2020-01-25 22:12:07 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     this.state = {
       orders: [], // array of work orders 
       ordersToRender: [], // work orders but sorted || filtered
-      workers: [], // array of workers
+      workers: {}, // object of workers
       searchStrings: '', 
       sortLatest: false,
       isLoading: false
@@ -29,11 +29,10 @@ class App extends Component {
   async fetchData() {
     await this.fetchWorkOrders(); // fetch work orders 
     const { orders, workers } = this.state;
-    for ( const o of orders ) { // for each order fetch worker if not in workers array already
+    for ( const o of orders ) { // for each order fetch worker if not in workers object already
       const wid = o.workerId;
-      const worker = workers.find( ({id}) => id === wid); // check if worker already exsist in array
-      if (worker) {
-        continue; // worker exsist in array
+      if (workers[wid]) {
+        continue; // worker exsist
       } else {
         await this.fetchWorker(wid);
       }
@@ -43,7 +42,7 @@ class App extends Component {
   async fetchWorkOrders() {
     try{
       this.setState({ isLoading: true });
-      const res = await fetch(ordersURL)
+      const res = await fetch(ordersURL);
       if (!res.ok) {
         throw Error(res.statusText);
       }
@@ -71,7 +70,7 @@ class App extends Component {
       }
       const json = await res.json();
       const worker = json.worker;
-      workers.push(worker);
+      workers[id] = worker;
       this.setState({
         workers: workers,
         isLoading: false
@@ -95,7 +94,7 @@ class App extends Component {
       <Container className="App mt-5">
         <ListCards
           orders={ordersToRender}
-          workers={workers}
+          workers={Object.values(workers)} // pass in an array of workers for the search bar
           searchStrings={searchStrings}
           onStringChange={this.handleStringChange}
           onToggleSwitch={this.handleSwitch} />
